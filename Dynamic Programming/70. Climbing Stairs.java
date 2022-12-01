59038f081b783bd3f3f196aac26cf514ef161b14
Tanujarora100 <108284546+Tanujarora100@users.noreply.github.com>
2022-12-01 13:06:17 +0530

    Create 70. Climbing Stairs.java

---
+++ Dynamic Programming/70. Climbing Stairs.java
@@ -0,0 +1,12 @@
+public int climbStairs(int n) {
+        int []dp= new int[n+1];
+      return climb(n, dp);
+    }
+  public int climb(int n, int[] dp)
+  {
+    if(n==1) return 1;
+    if(n==2) return 2;
+    if(dp[n]!=0)
+      return dp[n];
+    return dp[n]=climb(n-1,dp)+climb(n-2,dp);
+  }
