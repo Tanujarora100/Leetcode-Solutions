b60003d0b75c4dd8a0d7b5fc26dfe0d5d531bd4e
Tanujarora100 <108284546+Tanujarora100@users.noreply.github.com>
2022-12-02 15:28:30 +0530

    Update NearestSmallerElementInterviewBit.java

--- StacksandQueue/NearestSmallerElementInterviewBit.java
+++ StacksandQueue/NearestSmallerElementInterviewBit.java
@@ -2,13 +2,7 @@ public class Solution {
     public ArrayList<Integer> prevSmaller(ArrayList<Integer> A) {
         ArrayList<Integer> ans = new ArrayList<>();
         Stack<Integer> st = new Stack<>();
-        for (int i = 0; i < A.size(); i++) {
-            if(A.size()==1) 
-            {
-                ans.add(-1);
-                return ans;
-            }
-            //Edge Case.
+        for (int i = 0; i < A.size(); i++){
            while(!st.isEmpty() && A.get(i)<=st.peek())
             st.pop();
            if(!st.isEmpty())
