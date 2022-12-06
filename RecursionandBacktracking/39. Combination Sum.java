b23e78d8ea76ae52e08e9151e54e2eb1af97e248
Tanujarora100 <108284546+Tanujarora100@users.noreply.github.com>
2022-12-06 14:18:47 +0530

    Create 39. Combination Sum.java

---
+++ RecursionandBacktracking/39. Combination Sum.java
@@ -0,0 +1,18 @@
+class Solution {
+   public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        backtrack(list, new ArrayList<Integer>(), candidates, target, 0);
+        return list;
+    }
+    
+    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] cand, int remain, int start) {
+        if (remain < 0) return; /** no solution */
+        else if (remain == 0) list.add(new ArrayList<>(tempList));
+        else{
+            for (int i = start; i < cand.length; i++) { 
+                tempList.add(cand[i]);
+                backtrack(list, tempList, cand, remain-cand[i], i);
+                tempList.remove(tempList.size()-1);
+            } 
+        }
+    }}
