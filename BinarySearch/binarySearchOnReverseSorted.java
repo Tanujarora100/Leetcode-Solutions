5aecab0ecab64880c9b6f73ad1d49d28f7d11447
Tanujarora100 <108284546+Tanujarora100@users.noreply.github.com>
2022-12-28 16:08:23 +0530

    Create binarySearchOnReverseSorted.java

---
+++ BinarySearch/binarySearchOnReverseSorted.java
@@ -0,0 +1,15 @@
+  int binarysearch(int arr[], int n, int k) {
+        int start = 0;
+        int end = n - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == k)
+                return mid;
+            if (arr[mid] < k)
+                end = mid - 1;
+            else
+                start = mid + 1;
+        }
+        return -1;
+        // Reverse Order mei kardo simple
+    }
